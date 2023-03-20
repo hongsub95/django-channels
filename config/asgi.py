8a73ed2b6efdc556a779926dcca6637827a34877
hongsub <hong9506@gmail.com>
2023-03-20 23:14:07 +0900

    간단하게 Echo 웹소켓 구현

--- config/asgi.py
+++ config/asgi.py
@@ -9,12 +9,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
+
+from app import routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter({
     "http":django_asgi_app,
+    "websocket":URLRouter(
+        routing.websocket_urlpatterns
+    )
 })
